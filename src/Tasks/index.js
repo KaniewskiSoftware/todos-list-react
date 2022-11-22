d71ed014e626bba4fe77c274654c5fb4c16ac841
Patryk Kaniewski <kaniewski.patryk@gmail.com>
2022-11-22 16:30:50 +0100

    removeTask functionality added

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,11 +1,12 @@
 import "./style.css";
 
-const Tasks = ({ tasks, hideDone }) => (
+const Tasks = ({ tasks, hideDone, removeTask }) => (
   <ul className="tasks">
     {tasks.map((task) => (
       <li
         key={task.id}
-        className={`className= tasks__item ${task.done && hideDone ? "tasks__item--hidden" : ""}`}
+        className={`className= tasks__item ${task.done && hideDone ? "tasks__item--hidden" : ""
+        }`}
       >
         <button className="tasks__button tasks__button--toggleDone">
           {task.done ? "✔️" : ""}
@@ -13,7 +14,12 @@ const Tasks = ({ tasks, hideDone }) => (
         <p className={`tasks__text ${task.done ? "tasks__text--done" : ""}`}>
           {task.content}
         </p>
-        <button className="tasks__button tasks__button--remove">🗑️</button>
+        <button
+          className="tasks__button tasks__button--remove"
+          onClick={() => removeTask(task.id)}
+        >
+          🗑️
+        </button>
       </li>
     ))}
   </ul>
