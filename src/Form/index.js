38b7bd610912804f3ef30f49720dd87b56aebd27
Patryk Kaniewski <kaniewski.patryk@gmail.com>
2022-11-22 17:55:12 +0100

    addNewTask interaction and focus on input after render and after formsubmit

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,15 +1,36 @@
+import { useState, useRef, useEffect } from "react";
 import "./style.css";
 
-const Form = () => (
-  <form className="form">
-    <input
-      className="form__input"
-      type="text"
-      placeholder="Co jest do zrobienia?"
-      autoFocus
-    />
-    <button className="form__button">Dodaj zadanie</button>
-  </form>
-);
+const Form = ({ addNewTask }) => {
+  const [newTaskContent, setNewTaskContent] = useState("");
+  const ref = useRef();
+
+  useEffect(()=>{
+    ref.current.focus();
+  })
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    if (newTaskContent.trim()) {
+      addNewTask(newTaskContent.trim());
+    }
+    setNewTaskContent("");
+  };
+
+  return (
+    <form className="form" onSubmit={onFormSubmit}>
+      <input
+        ref={ref}
+        value={newTaskContent}
+        className="form__input"
+        type="text"
+        placeholder="Co jest do zrobienia?"
+        autoFocus
+        onChange={({ target }) => setNewTaskContent(target.value)}
+      />
+      <button className="form__button">Dodaj zadanie</button>
+    </form>
+  );
+};
 
 export default Form;
