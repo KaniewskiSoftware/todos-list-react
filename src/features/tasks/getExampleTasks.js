5e4285d9b77d7b9bac9989aaf35c770c6822f2a7
st4rkmano <117393647+st4rkmano@users.noreply.github.com>
2022-12-01 19:03:48 +0100

    await fetch added

--- src/features/tasks/getExampleTasks.js
+++ src/features/tasks/getExampleTasks.js
@@ -1,9 +1,9 @@
 export const getExampleTasks = async () => {
-  const response = fetch("/todos-list-react/exampleTasks.json");
+  const response = await fetch("/todos-list-react/exampleTasks.json");
 
   if (!response.ok) {
     new Error(response.statusText);
   }
-//   console.log(await(await response).json());
-  return await (await response).json();
+  
+  return await response.json();
 };
