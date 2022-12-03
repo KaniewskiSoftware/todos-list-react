e71857aba97e82c4a3e78c4a0d8c1357d48bb3d1
st4rkmano <117393647+st4rkmano@users.noreply.github.com>
2022-12-03 10:56:09 +0100

    author page content added and styled.

--- src/features/author/AuthorPage.js
+++ src/features/author/AuthorPage.js
@@ -1,19 +1,25 @@
 import Container from "../../common/Container";
 import Header from "../../common/Header";
 import Section from "../../common/Section";
+import { Paragraph } from "./styled";
 
 function Author() {
-    return (
-        <Container>
-          <Header title="O autorze" />
-          <Section 
-          title = "Patryk Kaniewski"
-          body={
-              <>nanana</>
-          }
-          />
-        </Container>
-      )
+  return (
+    <Container>
+      <Header title="O autorze" />
+      <Section
+        title="Patryk Kaniewski"
+        body={
+          <Paragraph>
+            <strong>Cześć!</strong><br/>
+            W tym momencie uczę się programować. Wiążę z tym swoją najbliższą
+            przyszłość. Oprócz tego sprawia mi to ogrom frajdy. Dzięki, że
+            zainteresowałeś się tą podstroną. <strong>Pozdrawiam serdecznie!</strong>.
+          </Paragraph>
+        }
+      />
+    </Container>
+  );
 }
 
 export default Author;
---
+++ src/features/author/styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Paragraph = styled.p`
+  margin: 0;
+  padding: 20px;
+  line-height: 1.4;
+`;
